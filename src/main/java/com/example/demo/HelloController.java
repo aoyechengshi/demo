80f1dbf441eb3cefe80a016846ed418fbd66f0d4
ZiSEI <aoyechengshi@gmail.com>
2026-03-14 23:55:40 +0900

    feat: 名前入力フォームを実装

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,13 +3,20 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
 	@GetMapping("/")
-	public String hello(Model model) {
-		model.addAttribute("name", "田中");
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping("/hello")
+	public String hello(@RequestParam String name, Model model) {
+		model.addAttribute("name", name);
 		return "hello";
 	}
 }
